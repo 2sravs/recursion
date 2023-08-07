3dae4da9958202e11e5bf91cdc22c6f7986afcba
2Sravs <sravani.chekuri1243#gmail.com>
2023-08-07 15:35:48 +0530

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+
+             void printViaRecursion(string[] names, int index)
+            {
+                if (index < names.Length)
+                {
+                    Console.WriteLine(names[index]);
+                    printViaRecursion(names, index + 1);
+                }
+            }
+
+             void runApp()
+            {
+                string[] students = { "Alice", "Bob", "Charlie", "David", "Eve" };
+                Console.WriteLine("Names of Students:");
+
+                
+                printViaRecursion(students, 0);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
+   
